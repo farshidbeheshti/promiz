67c998a40d9a0a115175d839a118ade86cfc0a90
Farshid Beheshti <farshid.beheshti@gmail.com>
2024-01-20 01:20:41 +0330

    feat: Building for the esm module format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-class Promiz {
-  constructor(resolver) {
-    if (resolver !== "function") {
-      throw new TypeError("Resolver is not a function");
+export default class Promiz {
+  constructor(executor) {
+    if (typeof executor !== "function") {
+      throw new TypeError("Executor is not a function");
     }
   }
+  test() {
+    return false;
+  }
 }
-
-module.exports = Promiz;
