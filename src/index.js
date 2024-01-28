de87269722cfc7e540aadccca6304243459a1be5
Farshid Beheshti <farshid.beheshti@gmail.com>
2024-01-28 23:51:41 +0330

    feat: implementing section stated by 27.2.3-"The Promise Constructor" of the spec

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
+import InternalSlots from "./internal-slots";
+import { createResolvingFunctions } from "./operations";
+
 export default class Promiz {
   constructor(executor) {
+    if (typeof executor === "undefined") {
+      throw new TypeError("Executor is undefined.");
+    }
     if (typeof executor !== "function") {
       throw new TypeError("Executor is not a function.");
+    }
+    this[InternalSlots.state] = "pending";
+    this[InternalSlots.result] = undefined;
+    this[InternalSlots.isHandled] = false;
+    this[InternalSlots.fulfillReactions] = [];
+    this[InternalSlots.rejectReactions] = [];
+    const { resolve, reject } = createResolvingFunctions(this);
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
     }
   }
 }
